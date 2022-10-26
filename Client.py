2879e7a778d4d7a960238b2d14632c8ca5ad48e6
Taner Şan <79445142+Tnrsn@users.noreply.github.com>
2022-10-26 13:47:56 +0300

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,31 +1,10 @@
-# import socket
-
-# PORT = 4545
-
-# Disconnect_msg = "!DISCONNECT"
-# SERVER = socket.gethostbyname(socket.gethostname())
-
-# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# client.connect((SERVER, PORT))
-
-
-# def send_msg(msg):
-#     message = msg.encode('utf-8')
-#     client.send(message)
-
-
-# while True:
-#     msg = input()
-#     send_msg(msg)
-
-
-from concurrent.futures import thread
 import socket
 import threading
 
 PORT = 4545
 
 Disconnect_msg = "!DISCONNECT"
+#Server = "10.1.55.12"
 SERVER = socket.gethostbyname(socket.gethostname())
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -38,13 +17,10 @@ def send_msg():
     client.send(message)
 
 def recieveMsg():
-    print(client.recv(1024).decode())
+    print("\n", client.recv(1024).decode())
 
 while True:
     recieveThread = threading.Thread(target=recieveMsg, args=())
     sendThread = threading.Thread(target=send_msg, args=())
     recieveThread.start()
     sendThread.start()
-    
-    # send_msg()
-    # print(client.recv(1024).decode())
