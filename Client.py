a09d4b2a1cce473d3c2e85b8a506b8dbe4fba368
Taner Şan <79445142+Tnrsn@users.noreply.github.com>
2022-12-14 13:24:30 +0300

    Update Client.py

--- Client.py
+++ Client.py
@@ -10,25 +10,23 @@ root = tk.Tk()
 entry = tk.Entry()
 entry.pack()
 
-listbox = tk.Listbox(root, width=100, height=50)
+listbox = tk.Listbox(root, width=100, height=20)
 listbox.pack()
 
 PORT = 4545
 
-SERVER = "10.1.56.53"
+SERVER = "10.1.55.12"
 #SERVER = socket.gethostbyname(socket.gethostname())
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER, PORT))
 
 def send_msg(event = None):
-    # msg = input()
-    print(entry.get())
     msg = entry.get()
+    entry.delete(0, "end")
     message = msg.encode('utf-8')
     client.send(message)
 
-entry.bind("<Return>", send_msg)
 
 def recieveMsg():
     while(True):
@@ -39,10 +37,11 @@ def recieveMsg():
             else:
                 # Add a value to the listbox
                 listbox.insert(tk.END, recievedMsg)
-                # print(recievedMsg)
 
         except:
             listbox.insert(tk.END, "Disconnected from the server!")
+
+entry.bind("<Return>", send_msg)
 
 button = tk.Button(text="Submit", command=send_msg)
 button.pack()
@@ -52,10 +51,3 @@ recieveThread.start()
 
 GUIThread = threading.Thread(target=root.mainloop(), args=())
 GUIThread.start()
-
-# while True:
-#     recieveThread = threading.Thread(target=recieveMsg, args=())
-#     # sendThread = threading.Thread(target=send_msg, args=())
-#     recieveThread.start()
-#     # sendThread.start()
-
