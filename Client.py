595863d3c3229780fe24d4a07909a3356d40836e
Selim SARIKAYA <slmsky24@gmail.com>
2023-03-08 09:14:21 +0300

    update_textbox

--- Client.py
+++ Client.py
@@ -3,6 +3,7 @@ import threading
 import sys, subprocess
 import tkinter as tk
 
+<<<<<<< Updated upstream
 root = tk.Tk() # create a Tkinter window
 root.title(" TCP - IP Sohbet Programı") # determine the title of the window
 root.geometry('400x300') # determine the weight and height of the window
@@ -15,6 +16,19 @@ messageBox.pack(expand = True) # add the messageBox widget to the window
 entry = tk.Entry() # create a Tkinter Entry widget for user input
 entry.configure(bg = '#512B58', fg = 'white')
 entry.pack(expand = True) # add the Entry widget to the window
+=======
+root = tk.Tk()
+root.title("TApp")
+root.geometry('600x400') # determine the weight and height of the window
+root.configure(bg = '#212121') # determine the background color of the window
+
+# create a Tkinter messageBox widget to display received messages 
+messageBox = tk.Listbox(root, width = 60, height = 10, bg = '#303030', fg = 'white', borderwidth = 0) 
+messageBox.pack(padx = 10, pady = 10, fill = tk.BOTH, expand = True) # add the messageBox widget to the window
+
+entry = tk.Entry(root, bg = '#303030', fg = 'white', borderwidth = 0, font = ("default", 20))
+entry.pack(side = tk.LEFT, padx = 10, pady = 10, fill = tk.BOTH, expand = True) # add the Entry widget to the window
+>>>>>>> Stashed changes
 
 PORT = 4545
 
@@ -29,7 +43,11 @@ def send_msg(event = None):
     client.send(message) # send the message to the server
 
 def clear_msg(event = None):
+<<<<<<< Updated upstream
     messageBox.delete(0, 'end')
+=======
+    messageBox.delete(0, 'end') #Clears messagebox
+>>>>>>> Stashed changes
 
 def recieveMsg():
     while(True):
@@ -40,6 +58,7 @@ def recieveMsg():
         except:
             messageBox.insert(tk.END, "Disconnected from the server!") # if an exception occurs, display an error message in the messageBox widget
 
+<<<<<<< Updated upstream
 entry.bind("<Return>", send_msg) # bind the "Return" key to the send_msg function
 
 button = tk.Button(text="SUBMIT", command=send_msg) # create a Tkinter Button widget to send the message
@@ -49,6 +68,19 @@ button.pack(expand = True) # add the Button widget to the window
 reset = tk.Button(text="CLEAR", command=clear_msg) # create a Tkinter Button widget to send the message
 reset.configure(bg = '#A20025', fg = 'white')
 reset.pack(expand = True) # add the Button widget to the window
+=======
+# #SUBMIT BUTTON
+entry.bind("<Return>", send_msg) # bind the "Return" (aka "Enter") key to the send_msg function
+button = tk.Button(root, text = "Submit", command = send_msg, bg = '#4CAF50', fg = 'white', borderwidth = 0) # create a Tkinter Button widget to send the message
+button.pack(side = tk.LEFT, padx = 0, pady = 0, fill = tk.BOTH, expand = True) # add the Button widget to the window
+
+# #CLEAR BUTTON
+reset = tk.Button(root, text = "Clear", command = clear_msg, bg = '#f44336', fg = 'white', borderwidth = 0)
+reset.pack(side = tk.LEFT, padx = 0, pady = 0, fill = tk.BOTH, expand = True)
+
+receive_thread = threading.Thread(target = receive_msg, args = ()) # create a thread for receiving messages
+receive_thread.start()
+>>>>>>> Stashed changes
 
 recieveThread = threading.Thread(target=recieveMsg, args=()) # create a thread for receiving messages
 recieveThread.start() # start the thread
