34a5fc5e05d459330c52215be72d181a0c5095ff
Selim SARIKAYA <slmsky24@gmail.com>
2023-03-08 09:22:15 +0300

    update_textbox

--- Client.py
+++ Client.py
@@ -3,22 +3,8 @@ import threading
 import sys, subprocess
 import tkinter as tk
 
-<<<<<<< Updated upstream
-root = tk.Tk() # create a Tkinter window
-root.title(" TCP - IP Sohbet Programı") # determine the title of the window
-root.geometry('400x300') # determine the weight and height of the window
-root.configure(bg = '#066595') # determine the background color of the window
-
-messageBox = tk.Listbox(root, width = 60, height = 10) # create a Tkinter messageBox widget to display received messages
-messageBox.configure(bg = '#512B58', fg = 'white')
-messageBox.pack(expand = True) # add the messageBox widget to the window
-
-entry = tk.Entry() # create a Tkinter Entry widget for user input
-entry.configure(bg = '#512B58', fg = 'white')
-entry.pack(expand = True) # add the Entry widget to the window
-=======
-root = tk.Tk()
-root.title("TApp")
+root = tk.Tk() # create window object
+root.title("TApp") # determine the title of the window
 root.geometry('600x400') # determine the weight and height of the window
 root.configure(bg = '#212121') # determine the background color of the window
 
@@ -26,11 +12,11 @@ root.configure(bg = '#212121') # determine the background color of the window
 messageBox = tk.Listbox(root, width = 60, height = 10, bg = '#303030', fg = 'white', borderwidth = 0) 
 messageBox.pack(padx = 10, pady = 10, fill = tk.BOTH, expand = True) # add the messageBox widget to the window
 
+# create a Tkinter entry widget
 entry = tk.Entry(root, bg = '#303030', fg = 'white', borderwidth = 0, font = ("default", 20))
 entry.pack(side = tk.LEFT, padx = 10, pady = 10, fill = tk.BOTH, expand = True) # add the Entry widget to the window
->>>>>>> Stashed changes
 
-PORT = 4545
+PORT = 4545 # listening port
 
 SERVER = socket.gethostbyname(socket.gethostname()) # get the IP address of the local machine for debugging
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a socket object for the client
@@ -43,11 +29,7 @@ def send_msg(event = None):
     client.send(message) # send the message to the server
 
 def clear_msg(event = None):
-<<<<<<< Updated upstream
-    messageBox.delete(0, 'end')
-=======
-    messageBox.delete(0, 'end') #Clears messagebox
->>>>>>> Stashed changes
+    messageBox.delete(0, 'end') # clear the messagebox
 
 def recieveMsg():
     while(True):
@@ -58,29 +40,17 @@ def recieveMsg():
         except:
             messageBox.insert(tk.END, "Disconnected from the server!") # if an exception occurs, display an error message in the messageBox widget
 
-<<<<<<< Updated upstream
-entry.bind("<Return>", send_msg) # bind the "Return" key to the send_msg function
-
-button = tk.Button(text="SUBMIT", command=send_msg) # create a Tkinter Button widget to send the message
-button.configure(bg = '#60A917', fg = 'white')
-button.pack(expand = True) # add the Button widget to the window
-
-reset = tk.Button(text="CLEAR", command=clear_msg) # create a Tkinter Button widget to send the message
-reset.configure(bg = '#A20025', fg = 'white')
-reset.pack(expand = True) # add the Button widget to the window
-=======
 # #SUBMIT BUTTON
 entry.bind("<Return>", send_msg) # bind the "Return" (aka "Enter") key to the send_msg function
 button = tk.Button(root, text = "Submit", command = send_msg, bg = '#4CAF50', fg = 'white', borderwidth = 0) # create a Tkinter Button widget to send the message
 button.pack(side = tk.LEFT, padx = 0, pady = 0, fill = tk.BOTH, expand = True) # add the Button widget to the window
 
 # #CLEAR BUTTON
-reset = tk.Button(root, text = "Clear", command = clear_msg, bg = '#f44336', fg = 'white', borderwidth = 0)
-reset.pack(side = tk.LEFT, padx = 0, pady = 0, fill = tk.BOTH, expand = True)
+reset = tk.Button(root, text = "Clear", command = clear_msg, bg = '#f44336', fg = 'white', borderwidth = 0) # create a Tkinter Button widget to clear the message
+reset.pack(side = tk.LEFT, padx = 0, pady = 0, fill = tk.BOTH, expand = True) # add the Button widget to the window
 
 receive_thread = threading.Thread(target = receive_msg, args = ()) # create a thread for receiving messages
-receive_thread.start()
->>>>>>> Stashed changes
+receive_thread.start() # start the thread
 
 recieveThread = threading.Thread(target=recieveMsg, args=()) # create a thread for receiving messages
 recieveThread.start() # start the thread
