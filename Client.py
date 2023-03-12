d52a81987ffade47b7da9ffa336a5e6a7bcdd25a
Selim SARIKAYA <slmsky24@gmail.com>
2023-03-12 12:51:28 +0300

    update ip address

--- Client.py
+++ Client.py
@@ -1,22 +1,27 @@
 import socket
 import threading
+import customtkinter
 import tkinter as tk
+import datetime
+import time
 
-root = tk.Tk()
-root.title("TApp")
-root.geometry('600x500')
-root.configure(bg='#212121')
+root = tk.Tk() # Create a Tkinter form object
+root.title("TCP - IP Chat App") # Determine the title of the Tkinter form object
+root.geometry('800x600') # Determine the window size of the Tkinter form object
+root.configure(bg='#212121') # Determine the background of the Tkinter form object
 
 # Create a Tkinter Listbox widget to display received messages
 messageBox = tk.Listbox(root, width=80, height=15, bg='#303030', fg='white', borderwidth=0, font=("default", 15))
 messageBox.pack(padx=20, pady=(20, 0), fill=tk.BOTH, expand=True)
 
 # Create a Tkinter Entry widget for the user to enter messages
-entry = tk.Entry(root, bg='#424242', fg='white', borderwidth=0, font=("default", 12), width=50)
-entry.pack(side=tk.LEFT, padx=(20, 0), pady=(3, 10), fill=tk.BOTH, expand=True)
+entry = customtkinter.CTkEntry(master=root,width=500,corner_radius=8)
+entry.pack(side=tk.LEFT, padx=(20, 0), pady=(10, 10), fill=tk.BOTH, expand=True)
 
 PORT = 4545 # Listening port
-SERVER = socket.gethostbyname(socket.gethostname()) # Get the IP address of the local machine for debugging
+
+#SERVER = socket.gethostbyname(socket.gethostname()) # Get the IP address of the local machine for debugging
+SERVER = '26.40.110.128' # Get the IP address of the your server machine
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a socket object for the client
 client.connect((SERVER, PORT)) # Connect the client socket to the server socket
@@ -30,32 +35,57 @@ def send_msg(event=None):
 
 # Function to clear the messageBox widget
 def clear_msg(event=None):
-    messageBox.delete(0, 'end')
+    messageBox.delete(0, 'end') # Clear the messages
+
+# Function to find the number of messages
+def count():
+    counter = 0 # Create variable for message count
+    while True:
+        # Works until end of messagebox
+        if messageBox.get(counter) != '':
+            counter = counter + 1 # Increases the variable's value by 1
+        else:
+            break # It stops when it reaches the end of the messagebox
+    return counter # Return variable's value
+
+# Function to backup the messages
+def backup_msg():
+    time = datetime.datetime.now() # Find the current time
+    today = str(datetime.date.today()) # Find the today date
+    file_name = "backup-" + today + time.strftime("-%H-%M-%S") + ".txt" # Create a file name according to the now time
+    file = open(file_name, "w") # Create a file
+    for item in range(1, count()):
+        if messageBox.get(item) != "Enter a real name with the /setname command!":
+            file.write(messageBox.get(item) + "\n") # Backup the message
+    file.close() # Close the file
 
 # Function to receive messages from the server
 def receive_msg():
+    m_control = True # Create a variable to display the error message once
     while True:
         try:
-            # Receive a message from the server
-            received_msg = client.recv(1024).decode()
-            # Add the message to the messageBox widget
-            messageBox.insert(tk.END, received_msg)
-            #Scroll down to new message
-            messageBox.see(tk.END)
+            received_msg = client.recv(1024).decode() # Receive a message from the server
+            messageBox.insert(tk.END, received_msg) # Add the message to the messageBox widget
+            messageBox.see(tk.END) # Scroll down to new message
         except:
-            # If an exception occurs, display an error message in the messageBox widget
-            messageBox.insert(tk.END, "Disconnected from the server!")
+            if m_control:
+                messageBox.insert(tk.END, "Disconnected from the server!") # If an exception occurs, display an error message in the messageBox widget
+                m_control = False # The message is write once
 
 # Bind the "Return" (aka "Enter") key to the send_msg function
 entry.bind("<Return>", send_msg)
 
-# Create a Tkinter Button widget to send messages
-button = tk.Button(root, text="Submit", command=send_msg, bg='#00BFFF', fg='white', borderwidth=0, padx=1, pady=1, font=("default", 12))
-button.pack(side=tk.LEFT, padx=(0, 20), pady=(3, 10), fill=tk.BOTH, expand=True)
+# Create a CustomTkinter Button widget to send messages
+button = customtkinter.CTkButton(master=root,width=50,fg_color='#0078FF',text_color='white',text='Submit',font=("default", 12),hover=False,command=send_msg)
+button.pack(side=tk.LEFT, padx=5, pady=(10, 10), fill=tk.BOTH, expand=True)
 
-# Create a Tkinter Button widget to clear messages
-# reset = tk.Button(root, text="Clear", command=clear_msg, bg='#F44336', fg='white', borderwidth=0, font=("default", 12))
-# reset.pack(side=tk.LEFT, padx=20, pady=(0, 20), fill=tk.BOTH)
+# Create a CustomTkinter Button widget to clear messages
+reset = customtkinter.CTkButton(master=root,width=50,fg_color='#F44336',text_color='white',text='Clear',font=("default", 12),hover=False,command=clear_msg)
+reset.pack(side=tk.LEFT, padx=0, pady=(10, 10), fill=tk.BOTH, expand=True)
+
+# Create a CustomTkinter Button widget to backup messages
+backup = customtkinter.CTkButton(master=root,width=50,fg_color='#8A2BE2',text_color='white',text='Backup',font=("default", 12),hover=False,command=backup_msg)
+backup.pack(side=tk.LEFT, padx=(5,20), pady=(10, 10), fill=tk.BOTH, expand=True)
 
 # Start a new thread to receive messages from the server
 receive_thread = threading.Thread(target=receive_msg)
--- Server.py
+++ Server.py
@@ -1,83 +1,112 @@
 import socket
 import threading
-# set up the port and host
+
+# Set up the port and host
 PORT = 4545
 HOST = socket.gethostbyname(socket.gethostname())
-# create a socket object
+
+# Create a socket object
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# bind the socket object to the specified address
+
+# Bind the socket object to the specified address
 ADDR = (HOST, PORT)
 server.bind(ADDR)
-# listen for incoming connections on the specified port
+
+# Listen for incoming connections on the specified port
 server.listen(1)
-# list to hold all connected clients
+
+# List to hold all connected clients
 conns = []
-# function to handle each client
+
+# Checks if the entered value is empty
+def name_control(name):
+    control = True
+    if name == '':
+        control = False
+    if len(name) > 0:
+        for item in name:
+            if item == " ":
+                control = False
+    return control
+
+# Function to handle each client
 def handle_client(conn, addr):
     try:
-        # indicate that a client has connected
+        # Indicate that a client has connected
         print(f"{addr} connected to server!")
-        # set flag to indicate client is connected
+        # Set flag to indicate client is connected
         connected = True
-        # send prompt to client to enter their name
+        # Send prompt to client to enter their name
         if connected:
             conn.send(bytes("Write your name:", "utf-8"))
-            # receive client's name and decode it
+            # Receive client's name and decode it
             name = conn.recv(360).decode('utf-8')
-            # add the client's connection to the list of connections
+            # Add the client's connection to the list of connections
             conns.append(conn)
-            # send message to all connected clients that a new client has joined
+            # Send message to all connected clients that a new client has joined
             for c in conns:
                 c.send(bytes(name + " joined to the server!", "utf-8"))
-        # loop to handle client messages
+        # Loop to handle client messages
         while connected:
-            # receive message from client and decode it
+            # Receive message from client and decode it
             msg = conn.recv(360).decode('utf-8')
             if msg:
-                # if message starts with '/', it is a command
+                # If message starts with '/', it is a command
                 if msg[0] == '/':
-                    # if command is 'setname', change the client's name
+                    # If command is 'setname', change the client's name
                     if msg[1:8] == 'setname':
                         name = msg[9:]
-                        # update client's connection in list of connections
-                        conns[conns.index(conn)] = conn
-                    # if command is 'commands', list available commands
-                    elif msg[1: 9] == 'commands':
-                        conn.send(bytes("-SERVER- Commands:\n1:  setname", "utf-8"))
-                    # otherwise, send invalid command message to client
+                        # If name is not null write new name
+                        if name_control(name):
+                            # Update client's connection in list of connections
+                            conns[conns.index(conn)] = conn
+                            conn.send(bytes('Your new name is ' + name + '', "utf-8"))
+                        # If name is null write error message
+                        else:
+                            conn.send(bytes("Enter a real name with the /setname command!", "utf-8"))
+                    # If command is 'commands', list available commands
+                    elif msg[1:9] == 'commands':
+                        conn.send(bytes("-SERVER- Commands:", "utf-8"))
+                        conn.send(bytes("1: setname", "utf-8"))
+                        conn.send(bytes("2: power", "utf-8"))
+                    # Otherwise, send invalid command message to client
                     else:
                         conn.send(bytes("-SERVER- (Invalid Command)", "utf-8"))
-                # if message is not a command and is less than 80 characters, send to all clients
+                # If message is not a command and is less than 80 characters, send to all clients
                 elif len(msg) < 80:
-                    for c in conns:
-                        if c == conn:
-                            c.send(bytes('YOU (' + name + '): ' + msg, "utf-8"))
-                        else:
-                            c.send(bytes(name + ': ' + msg, "utf-8"))
-                # if message is longer than 80 characters, send message limit exceeded message to client
+                    # If name is not null write message
+                    if name_control(name):
+                        for c in conns:
+                            if c == conn:
+                                c.send(bytes('YOU (' + name + '): ' + msg, "utf-8"))
+                            else:
+                                c.send(bytes(name + ': ' + msg, "utf-8"))
+                # If message is longer than 80 characters, send message limit exceeded message to client
                 else:
                     conn.send(bytes("Message limit exceeded! Letter limit is 80", "utf-8"))
     except:
-        # if an error occurs, send disconnect message to other clients and remove client's connection from list
+        # If an error occurs, send disconnect message to other clients and remove client's connection from list
         for c in conns:
             if c != conn:
                 c.send(bytes(f"{name} disconnected!", "utf-8"))
         conn.close()
-        # indicate that the client has disconnected
+        # Indicate that the client has disconnected
         print(f"{addr} disconnected!")
         conns.remove(conn)
-# function to start the server and listen for incoming connections
+
+# Function to start the server and listen for incoming connections
 def start():
-    # start listening for incoming connections
+    # Start listening for incoming connections
     server.listen()
-    # indicate that the server is running
+    # Indicate that the server is running
     print("Server is running...")
-    # loop to handle incoming connections
+    # Loop to handle incoming connections
     while True:
-        # accept incoming connection and create a new thread to handle it
+        # Accept incoming connection and create a new thread to handle it
         conn, ADDR = server.accept()
         thread = threading.Thread(target=handle_client, args=(conn, ADDR))
         thread.start()
+
 print("Server is starting...")
-#initilaizes the server
+#Initilaizes the server
 start()
